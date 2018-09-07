38c819a9be9619803df49f34cc4e7ce7268c4223
Alexandre Piza <alexandreapiza@gmail.com>
2018-09-07 15:51:11 -0300

    resolvido

--- Aula-1/exerc5.py
+++ Aula-1/exerc5.py
@@ -6,3 +6,9 @@
 #     [7,8,9]
 # ]
 #exemplo = 1+5+9+3+5+7
+linha1=[1,2,3]
+linha2=[4,5,6]
+linha3=[7,8,9]
+matriz=[linha1,linha2,linha3]
+result=matriz[0][0]+matriz[1][1]+matriz[2][2]+matriz[0][2]+matriz[1][1]+matriz[2][0]
+print(result)
